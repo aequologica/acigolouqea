1c0ba7c02dadeed4b9b083ec055d2581a7dd2fe1
Christophe Thiebaud <christophe.thiebaud@aequologica.net>
2015-07-14 09:50:52 +0200

    script to generate module pom chapeau

---
+++ CreateModulePom.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import argparse
+import os
+import pathlib
+import pystache
+import subprocess
+import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--directory",
+                    nargs='?',
+                    default=".",
+                    help="optional directory containing all projects, as direct sub-directories, to be grouped in a module pom")
+
+parser.add_argument("--artifactId",
+                    nargs='?',
+                    default="",
+                    help="optional module pom")
+
+args = parser.parse_args()
+
+basedir = args.directory
+artifactId = args.artifactId
+
+if basedir == '.':
+    basedir = os.path.dirname(os.path.realpath(basedir))
+    
+if artifactId == '':
+    artifactId = pathlib.Path(basedir).name
+    
+print("directory  : " + basedir+'\nartifactId : '+artifactId + '\n')
+
+def get_immediate_subdirectories_with_pom_as_modules(directory):
+    return [{'module':name} for name in os.listdir(directory)
+            if os.path.isdir(os.path.join(directory, name))
+            and pathlib.Path(os.path.join(directory, name), 'pom.xml').exists()]
+    
+class ModulePomFileTemplate(object):
+    def groupId(self):
+        return "net.aequologica.neo"
+    def artifactId(self):
+        return artifactId
+    def version(self):
+        return "1-SNAPSHOT"
+    def modules(self):
+        return get_immediate_subdirectories_with_pom_as_modules(basedir)
+
+modulePom = ModulePomFileTemplate()
+renderer = pystache.Renderer()
+print(renderer.render(modulePom))
+
+modulePomFile = os.path.join(basedir, 'modules.pom') 
+with open(modulePomFile, 'w') as file_:
+    file_.write(renderer.render(modulePom))
+
+# sys.exit(0)  
+
+# running   mvn validate with appropriately configured shakuntala will upload dag to hub
+# hub then is able to calculate topological sort that can be downloaded
+
+def runCommand(command):
+    try:
+        print ("---------------", command)
+        sys.stdout.flush()
+        subprocess.check_call(command, shell=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print ('\treturn code:', e.returncode)
+        print ('\toutput:', e.output)
+        sys.stdout.flush()
+        return False
+    
+def runCommandInDirectory(directory, command):
+    original_working_directory = os.getcwd()  # remember our original working directory
+    try :
+        os.chdir(str(directory))
+        print ("=======================", os.path.basename(os.getcwd()))
+        sys.stdout.flush()
+        return runCommand(command)
+    finally:
+        os.chdir(original_working_directory)
+        
+validate = ['mvn', '-f', 'modules.pom', 'validate']
+
+clean = ['mvn', '-f', 'modules.pom', 'clean', '-DskipTests'] 
+
+runCommandInDirectory(basedir, validate)
