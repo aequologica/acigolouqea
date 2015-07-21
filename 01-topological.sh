fb9be02efc3987129ad6e3a60207bd55294982a0
Christophe Thiebaud <christophe.thiebaud@aequologica.net>
2015-07-21 13:39:43 +0200

    SCRIPT_DIR

--- 01-topological.sh
+++ 01-topological.sh
@@ -2,7 +2,8 @@
 #-vxe
 # echo off  > /dev/null
 if [ -z ${TOPO_DIR+x} ]; then echo "TOPO_DIR is not set; export TOPO_DIR="; exit; else echo "TOPO_DIR is '$TOPO_DIR'"; fi
-python ./CreateModulePom.py \
+if [ -z ${SCRIPT_DIR+x} ]; then echo "SCRIPT_DIR is not set; export SCRIPT_DIR="; exit; else echo "SCRIPT_DIR is '$SCRIPT_DIR'"; fi
+python $SCRIPT_DIR/CreateModulePom.py \
   $TOPO_DIR
 pushd $TOPO_DIR
 export TOPO_DIR_NAME=${PWD##*/}  
