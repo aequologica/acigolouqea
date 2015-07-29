65d108d44bf87e371101326c45d05c75b270b32d
Christophe Thiebaud <christophe.thiebaud@aequologica,net>
2015-07-29 03:39:08 +0200

    better shakuntala conf

--- 03-run-release-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -vx
-if [ -z ${TOPO_DIR+x} ]; then echo "TOPO_DIR is not set; export TOPO_DIR="; exit; else echo "TOPO_DIR is '$TOPO_DIR'"; fi
-# --dryRun \ 
-pushd $TOPO_DIR
-export TOPO_DIR_NAME=${PWD##*/}  
-popd
-python ./GeoReleaseScript.py \
-  $TOPO_DIR_NAME.SFSF.cmd \
-  1> logs/run-release-script-for-$TOPO_DIR_NAME-$(date -d "today" +"%Y%m%d%H%M").log 2>&1
